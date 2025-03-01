310ba9263a757f68ba39f0a7a647c7773c0c5523
Namit <yadavnamit605@gmail.com>
2025-03-01 13:06:07 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -24,7 +24,7 @@ const createSendToken = (user, statusCode, res, msg) => {
         ),
         httpOnly: true
     };
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = false;
     res.cookie('jwt', token, cookieOptions);
     res.cookie('user_id', user.id, cookieOptions);
 
